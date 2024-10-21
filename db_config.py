6169d8af8fa0d8565c3b90017afd0cb6c8e9adc9
Akshun <akshunchauhan099@gmail.com>
2024-10-20 19:08:31 -0600

    secured MongoURL

--- db_config.py
+++ db_config.py
@@ -1,8 +1,14 @@
 import os
 import pymongo
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # MongoDB Configuration
 def get_mongo_client():
     # MongoDB Atlas connection string from environment variable
-    mongo_uri = os.getenv("mongodb+srv://akshunchauhan099:FegZInm6MXJmc4hi@cluster0.b0kvq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongo_uri = os.getenv("MONGODB_URI")
+    if mongo_uri is None:
+        raise ValueError("The MongoDB connection string is not set in the environment variables.")
     return pymongo.MongoClient(mongo_uri)
