0662cfccd3eab6aa3acbcea2a7e0f53869cb1907
Akshun <akshunchauhan099@gmail.com>
2024-10-21 01:19:22 -0600

    updated db config

--- db_config.py
+++ db_config.py
@@ -1,6 +1,7 @@
 import os
 import pymongo
-
+# MongoDB Configuration
 def get_mongo_client():
-    mongo_uri = os.getenv("MONGO_URL")
+    # MongoDB Atlas connection string from environment variable
+    mongo_uri = os.getenv("mongodb+srv://akshunchauhan099:FegZInm6MXJmc4hi@cluster0.b0kvq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     return pymongo.MongoClient(mongo_uri)
