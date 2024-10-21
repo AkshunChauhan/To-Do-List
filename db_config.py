cc0f46657adb86158b16aef7f6147fd47b6965b0
Akshun <akshunchauhan099@gmail.com>
2024-10-20 19:26:38 -0600

    fixed secrate key for deploymnet

--- db_config.py
+++ db_config.py
@@ -1,14 +1,8 @@
-import os
 import pymongo
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
+import streamlit as st
 
 # MongoDB Configuration
 def get_mongo_client():
-    # MongoDB Atlas connection string from environment variable
-    mongo_uri = os.getenv("MONGODB_URI")
-    if mongo_uri is None:
-        raise ValueError("The MongoDB connection string is not set in the environment variables.")
+    # MongoDB Atlas connection string from Streamlit secrets
+    mongo_uri = st.secrets["mongodb"]["uri"]
     return pymongo.MongoClient(mongo_uri)
